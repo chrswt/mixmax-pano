c7e1ce56f31ef1c24055c11aa033b72de9e8a41d
Christopher Tham <chriswt91@gmail.com>
2017-02-17 17:55:07 -0800

    Implement self-signed https certificate for dev

--- server.js
+++ server.js
@@ -13,3 +13,17 @@ const corsOptions = {
 // Define API routes and middleware
 app.get('/typeahead', cors(corsOptions), require('./api/typeahead'));
 app.get('/resolver', cors(corsOptions), require('./api/resolver'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.listen(process.env.PORT || 9145);
+} else {
+  // Create self-signed https certificate
+  pem.createCertificate({ days: 1, selfSigned: true }, (err, keys) => {
+    if (err) throw err;
+    
+    https.createServer({
+      key: keys.serviceKey,
+      cert: keys.certificate
+    }, app).listen(process.env.PORT || 9145);
+  });
+}
