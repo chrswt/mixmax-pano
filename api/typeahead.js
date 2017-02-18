4005d97f87992e42e1a1d7044f80be432ea45a81
Christopher Tham <chriswt91@gmail.com>
2017-02-17 18:05:39 -0800

    Implement google places autocomplete api request

--- api/typeahead.js
+++ api/typeahead.js
@@ -5,15 +5,34 @@ module.exports = (req, res) => {
   // Extract trailing query term after slash command is resolved
   const term = req.query.text.trim();
 
+  // Default behavior before field is dirty
   if (!term) {
-    // Default behavior before field is dirty
     res.json([{
       title: '<i>(Enter an address or place name)</i>',
       text: ''
     }]);
     return;
-  } else {
-    // Handle Google Places autocomplete API call
-  }
 
+  // Handle Google Places autocomplete API call
+  } else {
+    request({
+      url: 'https://maps.googleapis.com/maps/api/place/autocomplete/json',
+      method: 'GET',
+      qs: {
+        input: term,
+        key: key.autocomplete_key
+      },
+      gzip: true,
+      json: true,
+      timeout: 10 * 1000 
+    }, (err, resp) => {
+      if (err) {
+        res.status(500).send('Error');
+        console.log(err);
+
+      } else {
+        console.log(resp.body.predictions);
+      }
+    });
+  }
 };
